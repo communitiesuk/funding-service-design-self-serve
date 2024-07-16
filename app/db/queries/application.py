a2c433446b4919f7c5383b75ef53d61a3a71fc4d
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2024-07-16 10:40:08 +0000

    fs-4533 clone component

--- app/db/queries/application.py
+++ app/db/queries/application.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 from app.db import db
 from app.db.models import Component
 from app.db.models import Form
@@ -35,3 +37,42 @@ def get_component_by_id(component_id: str) -> Component:
 def get_list_by_id(list_id: str) -> Lizt:
     lizt = db.session.query(Lizt).where(Lizt.list_id == list_id).one_or_none()
     return lizt
+
+
+def _initiate_cloned_component(clone: Component, source_id: str, new_page_id=None, new_theme_id=None):
+    clone.page_id = new_page_id
+    clone.theme_id = new_theme_id
+    clone.is_template = False
+    clone.source_template_id = source_id
+    clone.component_id = uuid4()
+    return clone
+
+
+def clone_single_component(component_id: str, new_page_id=None, new_theme_id=None) -> Component:
+    component_to_clone: Component = (
+        db.session.query(Component).where(Component.component_id == component_id).one_or_none()
+    )
+    db.session.expunge(component_to_clone)
+    clone = _initiate_cloned_component(component_to_clone, component_to_clone.component_id, new_page_id, new_theme_id)
+
+    db.session.add(clone)
+    db.session.commit()
+
+    return component_to_clone
+
+
+def clone_multiple_components(component_ids: list[str], new_page_id=None, new_theme_id=None) -> list[Component]:
+    components_to_clone: list[Component] = (
+        db.session.query(Component).filter(Component.component_id.in_(component_ids)).all()
+    )
+    db.session.expunge_all()  # TODO is this ok or do we need to expunge each one separately?
+    clones = [
+        _initiate_cloned_component(
+            clone=clone, source_id=clone.component_id, new_page_id=new_page_id, new_theme_id=new_theme_id
+        )
+        for clone in components_to_clone
+    ]
+    db.session.add_all(clones)
+    db.session.commit()
+
+    return clones
