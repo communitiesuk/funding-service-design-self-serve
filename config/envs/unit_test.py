6cdb8baa686e0357be6b56730938bab7e433fd96
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2024-07-18 09:29:49 +0000

    default db url

--- config/envs/unit_test.py
+++ config/envs/unit_test.py
@@ -13,5 +13,5 @@ class UnitTestConfig(Config):
     FSD_LOG_LEVEL = logging.DEBUG
 
     SQLALCHEMY_DATABASE_URI = getenv(
-        "DATABASE_URL", "postgresql://postgres:password@fab-db:5432/fab_unit_test"  # pragma: allowlist secret
+        "DATABASE_URL", "postgresql://postgres:postgres@127.0.0.1:5432/fab_unit_test"  # pragma: allowlist secret
     )
